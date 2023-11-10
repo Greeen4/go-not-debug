ce8a4661cd5fc3290c5c4c98d3322b46c65cd761
alexander.savin <alexander.savin@solidlab.ru>
2023-11-10 12:13:45 +0300

    gpt help 1

--- cmd/notifyaer/main.go
+++ cmd/notifyaer/main.go
@@ -1,23 +1,45 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"net/http"
+
+// 	// "golang.org/x/exp/slog"
+
+// 	"./routes"
+// 	"appsec-notifayer/internal/config"
+// )
+
+// func main() {
+// 	cfg := config.MustLoad()
+
+// 	fmt.Println(cfg)
+
+// 	Test()
+
+// 	router := SetupRoutes()
+
+// 	srv := &http.Server{
+// 		Addr:    cfg.Address,
+// 		Handler: router,
+// 	}
+// 	srv.ListenAndServe()
+// }
+// main.go
 package main
 
 import (
 	"fmt"
 	"net/http"
 
-	// "golang.org/x/exp/slog"
-
+	"./" // импортируем пакет, содержащий функцию SetupRoutes
 	"appsec-notifayer/internal/config"
 )
 
 func main() {
 	cfg := config.MustLoad()
-
 	fmt.Println(cfg)
-
-	Test()
-
 	router := SetupRoutes()
-
 	srv := &http.Server{
 		Addr:    cfg.Address,
 		Handler: router,
